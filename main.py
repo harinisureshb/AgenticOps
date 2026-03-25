9f797ba1b36886e431d48f7198684b829d587039
Akhil-Philips-genAi <akhil.sambaraju@philips.com>
2026-03-25 12:55:55 +0530

    added FAQs and skeleton code

---
+++ main.py
@@ -0,0 +1,22 @@
+# app.py
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+# from graph import graph
+
+app = FastAPI(title="AgenticOps API")
+
+class RequestModel(BaseModel):
+    query: str
+
+@app.get("/")
+def home():
+    return {"message": "AgenticOps API is running"}
+
+@app.post("/run-agent")
+async def run_agent(request: RequestModel):
+    result = await graph.ainvoke({"input": request.query})
+    return {
+        "query": request.query,
+        "response": result["output"]
+    }
